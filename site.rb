b8021f72a51a61fd7b4563386850f7193cb6b6b8
Andrew Thorpe <pyroka@gmail.com>
2016-08-01 00:22:34 +0100

    Site now parses the config and uses it to set instance variables

--- site.rb
+++ site.rb
@@ -1,14 +1,16 @@
+require 'yaml'
 require 'pp'
 require File::expand_path('./page')
 require File::expand_path('./erb_context')
 
 module Evander
+  
   class Site
 
     def initialize(root_dir)
+      _parse_config(root_dir)
       @root_dir = root_dir
       @top_level_pages = Page.get_sub_pages(root_dir)
-
       site_context = _create_site_context
       @top_level_pages.each do |page|
         page.create_context(site_context)
@@ -47,6 +49,17 @@ module Evander
         end
         Dir.chdir(page.title) do
           _render_page(subpage)
+        end
+      end
+    end
+
+    def _parse_config(dirname)
+      config_path = dirname + "/config.yaml";
+      if(File.exist?(config_path))
+        config = YAML.load_file(config_path)
+        config.each do |key, value|
+          instance_variable_set("@" + key, value)
+          self.class.send(:attr_reader, key)
         end
       end
     end
